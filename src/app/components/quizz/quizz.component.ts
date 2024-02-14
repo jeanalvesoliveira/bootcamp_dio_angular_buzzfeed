313ecfec88bc896726a9481a6d6ca33d6c54daa2
Jean Alves de Oliveira <jeanalvesoliveira@gmail.com>
2024-02-14 15:02:05 -0300

    Criando as regras do jogo

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import quizz_questions from "../../../assets/data/quizz_questions.json";
+
+@Component({
+  selector: 'app-quizz',
+  standalone: true,
+  imports: [],
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent implements OnInit{
+  title: string = 'Titulo'
+  
+  questions: any
+  questionSelected: any
+  
+  answers: string[] = []
+  answerSelected: string = ''
+  
+  questionIndex: number = 0
+  questionMaxIndex: number = 0
+  
+  finished: boolean = false
+
+  ngOnInit(): void {
+    if (quizz_questions) {
+      this.finished = false
+      this.title = quizz_questions.title
+
+      this.questions = quizz_questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0
+      this.questionMaxIndex = this.questions.length
+
+      
+    }
+  }
+}
